889f527c3d97d27c6a235dbd13ee014b9d04fc1e
staminhe42 <staminhe42@gmail.com>
2019-05-31 19:01:13 +0200

    la config

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
+#!/usr/bash
+
+
 virtualenv venv
 source venv/bin/activate
-pip install Django==2.2
-pip install django-ex==0.1
-pip install psycopg2==2.8.2
-pip install  pytz==2019.1
-pip install sqlparse==0.3.0
+pip3 install -r requirement.txt
+pip install dist/django-ex-0.1.tar.gz
